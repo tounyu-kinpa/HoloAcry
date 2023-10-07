f36a846fd67a42daf2254d33fa2dbcac2d7705d2
nakayuu803 <ynakamura704@gmail.com>
2023-10-07 14:30:43 +0900

    ファイルインポート時のエラーモーダル実装

--- Assets/Scripts/Dialog/Alert/ErrorAlert.cs
+++ Assets/Scripts/Dialog/Alert/ErrorAlert.cs
@@ -5,35 +5,45 @@ using UnityEngine.UI;
 
 public class ErrorAlert : MonoBehaviour
 {
-    public GameObject ErrorModal;
-    public Transform Canvas;
-    GameObject modal;
-    GameObject ModalText;
-    Text ErrorText;
+    public GameObject ErrorModal;   // インスペクターから代入するErroModalのPrefab
+    public Transform Canvas;        // インスペクターから代入する親オブジェクト
 
     public void ShowNoInformErrorModal()
     {
         //完了ボタンが押されたとき、記入漏れがあったらErrorModalを表示
-        modal = ErrorModal.transform.Find("Modal").gameObject;
-        ModalText = modal.transform.Find("Label").gameObject;
-        ErrorText = ModalText.GetComponent<Text>();
-        ErrorText.text = "全て選択してください";
+        Text NoInformErrorStatement = FindErrorModalText();
+        NoInformErrorStatement = "全て選択してください";
         Instantiate(ErrorModal, Canvas);
     }
 
     public void ShowNoSelectErrorModal()
     {
         //削除やカラー変更ボタンが押されたときオブジェクトが選択されていなかったらErrorModalを表示
-        modal = ErrorModal.transform.Find("Modal").gameObject;
-        ModalText = modal.transform.Find("Label").gameObject;
-        ErrorText = ModalText.GetComponent<Text>();
-        ErrorText.text = "オブジェクトを選択してください";
-        Instantiate(ErrorModal, Canvas);     
+        Text NoSelectErrorStatement = FindErrorModalText();
+        NoSelectErrorStatement = "すべて選択してください";
+        Instantiate(ErrorModal, Canvas);  
+    }
+
+    public void FileImportErrorModal(){
+        //インポートされたファイルがOBJファイル以外だとErrorModalを表示
+        Text FileImportStatement = FindErrorModalText();
+        FileImportStatement = "OBJファイルのみインポートが可能です";
+        Instantiate(ErrorModal, Canvas); 
+    }
+
+    public Text FindErrorModalText()
+    {
+        // ErrorModalのPrefabからエラー文を取得
+        GameObject modal = ErrorModal.transform.Find("Modal").gameObject;
+        GameObject ModalText = modal.transform.Find("Label").gameObject;
+        Text ErrorText = ModalText.GetComponent<Text>();
+
+        return(ErrorText);
     }
 
     public void DeleteErrorModal()
     {
-        //ErrorModalの了解ボタンが押されたら
+        //ErrorModalの了解ボタンが押されたらModalを削除
         Destroy(ErrorModal);
     }
 }
