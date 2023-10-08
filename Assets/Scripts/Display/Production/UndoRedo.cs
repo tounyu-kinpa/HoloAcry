673bff9d086b81905a538b5f68e47a81b1ee41ca
nakayuu803 <ynakamura704@gmail.com>
2023-10-08 17:12:01 +0900

    Tにするために一旦コミット

--- Assets/Scripts/Display/Production/UndoRedo.cs
+++ Assets/Scripts/Display/Production/UndoRedo.cs
@@ -1,3 +1,4 @@
+// using System.Runtime.Intrinsics.X86;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,7 @@ using DefaultNamespace;
 public class SelectedModel
 {
     public bool NowMaking = false;
+    public bool NowMerge;
 
     // Elementの名前を格納
     public string name;
@@ -29,97 +31,123 @@ public class SelectedModel
     public Color color;
 
     // selectedGameObjectsと一致するcreatedGameObjectsのインデックスを格納
-    public int ObjectNamber;
+    public int ObjectID;
 }
 
 public class UndoRedo : MonoBehaviour
 {
-    public static Stack<SelectedModel> undoStack = new Stack<SelectedModel>();
-    public static Stack<SelectedModel> redoStack = new Stack<SelectedModel>();
+    public static Stack<object> undoStack = new Stack<object>();
+    public static Stack<object> redoStack = new Stack<object>();
 
     public void Undo()
     {
         if(undoStack.Count >= 2){
             // Stackから取り出して代入する関数
-            SelectedModel UndoValue = undoStack.Pop();
+            object UndoValue = undoStack.Pop();
             //変更後の情報を取り出してredostackにpush
             redoStack.Push(UndoValue);
             //変更前の情報を取り出して、その情報をもとにUndo
             UndoValue = undoStack.Pop();
-            AssignPopValue (UndoValue);
+            SelectedModel SelectedModelUndoValue = (SelectedModel)UndoValue;
+            UndoRedoBranch (SelectedModelUndoValue);
         }
     }
 
     public void Redo()
     {
         // Stackから取り出して代入する関数
-        SelectedModel RedoValue = redoStack.Pop();
+        object RedoValue = redoStack.Pop();
         //取り出した変更後の情報をもとにRedo
-        AssignPopValue (RedoValue);
+        SelectedModel SelectedModelRedoValue = (SelectedModel)RedoValue;
+        UndoRedoBranch (SelectedModelRedoValue);
     }
 
     public static void Do()
     {
-        SelectedModel DoValue =  NewModel();
+        object DoValue =  NewModel(ProductionManager.selectedGameObjects[0]); 
 
         // 変更後のObjectの値をまとめてUndoStackにPush
-        DoValue.NowMaking = false;
         undoStack.Push(DoValue);
         // RedoをClear
         redoStack.Clear();
     }
-    public void AssignPopValue(SelectedModel PopValue)
-    {
-        //undoしたとき生成されたばかりだったら消去
-        GameObject Element = GameObject.Find(PopValue.name);
 
-        if (PopValue.NowMaking == true){
-            Destroy(Element);
-        }
-        else{
-            //Destroy後のUndo,Redoの処理
-            if (Element == null) {
-                CreateElementButton InstanceCreateElement = new CreateElementButton();
-                InstanceCreateElement.CreateElement();
-                Element = ProductionManager.selectedGameObjects[0];
-            }
-                
-            Element.tag = PopValue.elementType;
-            Element.transform.name = PopValue.name;
-            Element.transform.localPosition = PopValue.position;
-            Element.transform.localScale = PopValue.scale;
-            Element.transform.localEulerAngles = PopValue.rotate;
+    public void AssignPopValue(SelectedModel PopValue, GameObject Element)
+    {                
+        Element.tag = PopValue.elementType;
+        Element.transform.name = PopValue.name;
+        Element.transform.localPosition = PopValue.position;
+        Element.transform.localScale = PopValue.scale;
+        Element.transform.localEulerAngles = PopValue.rotate;
 
-            MeshFilter ElementMeshFilter = Element.GetComponent<MeshFilter>();
-            ElementMeshFilter.mesh.vertices = PopValue.meshVertices;
+        MeshFilter ElementMeshFilter = Element.GetComponent<MeshFilter>();
+        ElementMeshFilter.mesh.vertices = PopValue.meshVertices;
 
-            Material ElementRenderer = Element.GetComponent<Renderer>().material;
-            ElementRenderer.color = PopValue.color;
-        }
+        Material ElementRenderer = Element.GetComponent<Renderer>().material;
+        ElementRenderer.color = PopValue.color;
+
         undoStack.Push(PopValue);
     }
 
-    public static int FindMatchingObject()
-    {
-        int index = ProductionManager.createdGameObjects.FindIndex(x => x == ProductionManager.selectedGameObjects[0]);
+    public void UndoRedoBranch(SelectedModel PopValue)
+    { 
+        // 結合の値をPopした時
+        if(PopValue is List<SelectedModel>){
+            List<SelectedModel> PopValueList = (List<SelectedModel>)PopValue;
+            //NowMerge = true子をworkspaceへ出して親を消去(Undo)
+            if(PopValueList[0].NowMerge == true){
 
-        return (index);
+            }
+            //faleは子をselectedGameObjectにAddしてProdactionManager.MergeObjects()へ(Redo)
+            else{
+
+            }
+        }
+        else{
+            GameObject Element = FindMatchingObjectID((SelectedModel)PopValue.ObjectID);
+            //undoしたとき生成されたばかりだったら消去
+            if ((SelectedModel)PopValue.NowMaking == true){
+                Destroy(Element);
+                undoStack.Push(PopValue);
+            }
+            else{
+                //Destroy後のUndo,Redoの処理
+                if (Element == null) {
+                    CreateElementButton InstanceCreateElement = new CreateElementButton();
+                    InstanceCreateElement.CreateElement();
+                    int ElementID = ProductionManager.selectedGameObjects[0].GetInstanceID();
+                    ElementID = PopValue.ObjectID;
+                    Element = FindMatchingObjectID(PopValue.ObjectID);
+                    AssignPopValue(PopValue, Element);
+                }
+                else{
+                    AssignPopValue(PopValue, Element);
+                }
+            }
+        }
     }
 
-    public static SelectedModel NewModel()
+    public static GameObject FindMatchingObjectID(int instanceID)
+    {
+        int index = ProductionManager.createdGameObjects.FindIndex(x => x.GetInstanceID() == instanceID);
+
+        return (ProductionManager.createdGameObjects[index]);
+    }
+
+    public static SelectedModel NewModel(GameObject selectedGameObject)
     {
         SelectedModel NewValue = new SelectedModel();
-        NewValue.ObjectNamber = FindMatchingObject();
-        NewValue.name = ProductionManager.createdGameObjects[NewValue.ObjectNamber].transform.name;  
-        NewValue.elementType = ProductionManager.createdGameObjects[NewValue.ObjectNamber].tag;
-        NewValue.position = ProductionManager.createdGameObjects[NewValue.ObjectNamber].transform.localPosition;
-        NewValue.scale = ProductionManager.createdGameObjects[NewValue.ObjectNamber].transform.localScale;
-        NewValue.rotate = ProductionManager.createdGameObjects[NewValue.ObjectNamber].transform.localEulerAngles;
+        NewValue.ObjectID = selectedGameObject.GetInstanceID();
+        NewValue.name = selectedGameObject.transform.name;  
+        NewValue.elementType = selectedGameObject.tag;
+        NewValue.position = selectedGameObject.transform.localPosition;
+        NewValue.scale = selectedGameObject.transform.localScale;
+        NewValue.rotate = selectedGameObject.transform.localEulerAngles;
 
-        MeshFilter ElementMeshFilter = ProductionManager.createdGameObjects[NewValue.ObjectNamber].GetComponent<MeshFilter>();
+        MeshFilter ElementMeshFilter = selectedGameObject.GetComponent<MeshFilter>();
         NewValue.meshVertices = ElementMeshFilter.mesh.vertices;
 
-        Material ElementRenderer = ProductionManager.createdGameObjects[NewValue.ObjectNamber].GetComponent<Renderer>().material;
+        Material ElementRenderer = selectedGameObject.GetComponent<Renderer>().material;
         NewValue.color = ElementRenderer.color;
 
         return(NewValue);
@@ -128,9 +156,30 @@ public class UndoRedo : MonoBehaviour
     //オブジェクト生成した時の処理
     public static void Create()
     {
-        SelectedModel CreateValue =  NewModel();
+        SelectedModel CreateValue =  NewModel(ProductionManager.selectedGameObjects[0]);
         CreateValue.NowMaking = true;
         undoStack.Push(CreateValue);
         Do();
+    }
+
+    public static void Merge()
+    {
+        // 結合したオブジェクトをUndoStack.PushするためのList
+        List<SelectedModel> MergeVale = new List<SelectedModel>(); 
+        int ParentID = ProductionManager.selectedGameObjects[0].GetInstanceID(); 
+        GameObject ParentObject = FindMatchingObjectID(ParentID);
+        // 結合したオブジェクトの数
+        int childCount = ParentObject.transform.childCount;
+        MergeVale.Add(NewModel(ProductionManager.selectedGameObjects[0]));
+
+        for (int i = 0; i < childCount; i++){
+            GameObject childObject = ParentObject.transform.GetChild(i).gameObject;
+            MergeVale.Add(NewModel(childObject));
+        }
+
+        MergeVale[0].NowMerge = true;
+        undoStack.Push(MergeVale);
+        MergeVale[0].NowMerge = false;
+        undoStack.Push(MergeVale);      
     }
 }
