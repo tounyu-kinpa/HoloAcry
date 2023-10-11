4e92cbd46b4e36562bd54e513d1a2eb5a5933fd8
assam <assam5649.18@gmail.com>
2023-10-12 04:29:10 +0900

    Destroy Add

---
+++ .history/Assets/Scripts/Display/List/Destory_20231012023434.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Destory : MonoBehaviour
+{
+    public Transform content;
+
+    foreach(Transform child in content){
+        Destory(Child.GameObject);
+    }
+    
+}
---
+++ Assets/Scripts/Display/List/Destory.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Destory : MonoBehaviour
+{
+    public Transform content;
+
+    public void OnClick()
+    {
+        
+        foreach (Transform child in content)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+    
+}
