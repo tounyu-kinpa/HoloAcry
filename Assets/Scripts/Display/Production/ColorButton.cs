23a6944eaac5e6c45ded93423196c7f4dae77fb1
sungyo4869 <hchengjian1@gmail.com>
2023-10-12 23:38:41 +0900

    エラー処理の追加

--- Assets/Scripts/Display/Production/ColorButton.cs
+++ Assets/Scripts/Display/Production/ColorButton.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using Display.Production;
 
 public class SetColorButton : MonoBehaviour
 {
     public GameObject ColorUIPrefab;
     public Transform ProductionUI;
+    public ErrorAlert alert;
 
     private GameObject ColorSetUI;
 
@@ -13,7 +15,14 @@ public class SetColorButton : MonoBehaviour
         {
             return;
         }
-        
-        ColorSetUI = Instantiate(ColorUIPrefab, ProductionUI);
+
+        if (ProductionManager.selectedGameObjects.Count == 0)
+        {
+            alert.ShowNoSelectErrorModal(GlobalVariables.ParentsUI);
+        }
+        else
+        {
+            ColorSetUI = Instantiate(ColorUIPrefab, ProductionUI);
+        }
     }
 }
